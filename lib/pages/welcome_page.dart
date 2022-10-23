0bc1bc6618f9fe04c3bf775d85a8ce065a0e436b
Salmanul Farisi <salmanfarisi0027@gmail.com>
2022-10-23 20:58:52 +0530

    welcome page ui started

---
+++ lib/pages/welcome_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  List images = ["welcome1", "welcome2", "welcome3"];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: images.length,
+          itemBuilder: (_, index) {
+            return Container(
+              width: double.maxFinite,
+              height: double.maxFinite,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/${images[index]}.jpg"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            );
+          }),
+    );
+  }
+}
