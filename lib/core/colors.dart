66503da290f7ffd58a41cfa8545418b46dc9176c
Salmanul Farisi <salmanfarisi0027@gmail.com>
2022-10-23 22:23:56 +0530

    welcome page ui completed.created scroll dotscustomly

---
+++ lib/core/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  static const textColor1 = Color(0xFF989acd);
+  static const textColor2 = Color(0xFF878593);
+  static const bigTextColor = Color(0xFF2e2e31);
+  static const mainColor = Color(0xFF5d69b3);
+  static const starColor = Color(0xFFe7bb4e);
+  static const mainTextColor = Color(0xFFababad);
+  static const buttonBackgroung = Color(0xFFf1f1f0);
+}
--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app/core/colors.dart';
+import 'package:travel_app/widget/app_large_text.dart';
+import 'package:travel_app/widget/app_text.dart';
+import 'package:travel_app/widget/resposive_button.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({Key? key}) : super(key: key);
@@ -23,6 +27,56 @@ class _WelcomePageState extends State<WelcomePage> {
                 image: DecorationImage(
                   image: AssetImage("assets/${images[index]}.jpg"),
                   fit: BoxFit.cover,
+                ),
+              ),
+              child: Container(
+                margin: const EdgeInsets.only(top: 150, left: 20, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        AppLargeText(text: "Trips"),
+                        AppText(
+                          text: "Mountain",
+                          size: 30,
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        SizedBox(
+                          width: 250,
+                          child: AppText(
+                            text:
+                                "Mountain hikes give you an increadible sense of freedom along with endurance fest",
+                            color: AppColors.textColor2,
+                            size: 14,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 40,
+                        ),
+                        ResponsiveButton(
+                          width: 120,
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: List.generate(3, (indexDots) {
+                        return Container(
+                          margin: const EdgeInsets.only(bottom: 2),
+                          width: 8,
+                          height: index == indexDots ? 25 : 8,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              color: index == indexDots
+                                  ? AppColors.mainColor
+                                  : AppColors.mainColor.withOpacity(0.3)),
+                        );
+                      }),
+                    )
+                  ],
                 ),
               ),
             );
