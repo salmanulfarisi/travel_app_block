36485099b0a6452f179b7153de2317f87f468649
Salmanul Farisi <salmanfarisi0027@gmail.com>
2022-10-23 22:48:10 +0530

    bottom navigation bar ui page created

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:travel_app/pages/welcome_page.dart';
+import 'package:travel_app/pages/navpages/main_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const WelcomePage(),
+      home: const MainPage(),
     );
   }
 }
---
+++ lib/pages/navpages/main_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app/pages/navpages/bar_items_page.dart';
+import 'package:travel_app/pages/navpages/home_page.dart';
+import 'package:travel_app/pages/navpages/my_page.dart';
+import 'package:travel_app/pages/navpages/search_page.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  List pages = [
+    const HomePage(),
+    const BarItemPage(),
+    const SearchPage(),
+    const MyPage()
+  ];
+  int currentIndex = 0;
+  void onTap(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        onTap: onTap,
+        currentIndex: currentIndex,
+        selectedItemColor: Colors.black54,
+        unselectedItemColor: Colors.grey.withOpacity(0.5),
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        unselectedFontSize: 0,
+        selectedFontSize: 0,
+        elevation: 0,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.apps),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bar_chart_sharp),
+            label: "Bar",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: "Search",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "My",
+          ),
+        ],
+      ),
+    );
+  }
+}
